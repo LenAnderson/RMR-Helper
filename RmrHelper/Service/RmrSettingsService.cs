b066a6b79f441c8614ad3be635a82d47be38b96d
Len Anderson <Anderson.Len@outlook.com>
2022-06-20 13:26:52 +0200

    check if ini files exist

--- RmrHelper/Service/RmrSettingsService.cs
+++ RmrHelper/Service/RmrSettingsService.cs
@@ -16,40 +16,45 @@ namespace RmrHelper.Service
 		public void PopulateRmrSettings(string defaultsPath, string userPath, RmrSettingsViewModel settings)
 		{
 			var parser = new FileIniDataParser();
-			var ini = parser.ReadFile(defaultsPath);
-			var userIni = parser.ReadFile(userPath);
-			ini.Merge(userIni);
-
-			settings.NumberOfSliderSets = int.Parse(ini["Static"]["iNumberOfSliderSets"], CultureInfo.InvariantCulture);
-			settings.OverrideOnlyDoctorCanReset = int.Parse(ini["Override"]["iOnlyDoctorCanReset"], CultureInfo.InvariantCulture);
-			settings.OverrideIsAdditive = int.Parse(ini["Override"]["iIsAdditive"], CultureInfo.InvariantCulture);
-			settings.OverrideHasAdditiveLimit = int.Parse(ini["Override"]["iHasAdditiveLimit"], CultureInfo.InvariantCulture);
-			settings.OverrideAdditiveLimit = (int)float.Parse(ini["Override"]["fAdditiveLimit"], CultureInfo.InvariantCulture);
-
-			for (int i = 0; i < settings.NumberOfSliderSets; i++)
+			if (File.Exists(defaultsPath))
 			{
-				var section = ini[$"Slider{i}"];
-				var sliderSet = new SliderSetViewModel
-				{
-					Title = $"Slider Set {i + 1}",
-					SliderNames = section["sSliderName"],
-					TriggerName = section["sTriggerName"],
-					InvertTriggerValue = int.Parse(section["bInvertTriggerValue"] ?? "0") == 1,
-					TargetSizeIncrease = (int)float.Parse(section["fTargetMorph"], CultureInfo.InvariantCulture),
-					LowerThreshold = (int)float.Parse(section["fThresholdMin"], CultureInfo.InvariantCulture),
-					UpperThreshold = (int)float.Parse(section["fThresholdMax"], CultureInfo.InvariantCulture),
-					ArmorSlotsToUnequip = section["sUnequipSlot"],
-					UnequipThreshold = (int)float.Parse(section["fThresholdUnequip"] ?? "0", CultureInfo.InvariantCulture),
-					OnlyDoctorCanReset = int.Parse(section["bOnlyDoctorCanReset"] ?? "0") == 1,
-					IsAdditive = int.Parse(section["bIsAdditive"] ?? "0") == 1,
-					HasAdditiveLimit = int.Parse(section["bHasAdditiveLimit"] ?? "0") == 1,
-					AdditiveLimit = (int)float.Parse(section["fAdditiveLimit"] ?? "0", CultureInfo.InvariantCulture)
-				};
-				sliderSet.UpdateType = sliderSet.UpdateTypeList.FirstOrDefault(it => it.Item1 == int.Parse(section["iUpdateType"]));
-				sliderSet.ApplyTo = sliderSet.ApplyToList.FirstOrDefault(it => it.Item1 == int.Parse(section["iApplyTo"] ?? "-1"));
-				sliderSet.Sex = sliderSet.SexList.FirstOrDefault(it => it.Item1 == int.Parse(section["iSex"] ?? "-1"));
+				var ini = parser.ReadFile(defaultsPath);
+				if (File.Exists(userPath)) { 
+				var userIni = parser.ReadFile(userPath);
+				ini.Merge(userIni);
 
-				settings.AddSliderSet(sliderSet);
+				settings.NumberOfSliderSets = int.Parse(ini["Static"]["iNumberOfSliderSets"], CultureInfo.InvariantCulture);
+				settings.OverrideOnlyDoctorCanReset = int.Parse(ini["Override"]["iOnlyDoctorCanReset"], CultureInfo.InvariantCulture);
+				settings.OverrideIsAdditive = int.Parse(ini["Override"]["iIsAdditive"], CultureInfo.InvariantCulture);
+				settings.OverrideHasAdditiveLimit = int.Parse(ini["Override"]["iHasAdditiveLimit"], CultureInfo.InvariantCulture);
+				settings.OverrideAdditiveLimit = (int)float.Parse(ini["Override"]["fAdditiveLimit"], CultureInfo.InvariantCulture);
+
+					for (int i = 0; i < settings.NumberOfSliderSets; i++)
+					{
+						var section = ini[$"Slider{i}"];
+						var sliderSet = new SliderSetViewModel
+						{
+							Title = $"Slider Set {i + 1}",
+							SliderNames = section["sSliderName"],
+							TriggerName = section["sTriggerName"],
+							InvertTriggerValue = int.Parse(section["bInvertTriggerValue"] ?? "0") == 1,
+							TargetSizeIncrease = (int)float.Parse(section["fTargetMorph"], CultureInfo.InvariantCulture),
+							LowerThreshold = (int)float.Parse(section["fThresholdMin"], CultureInfo.InvariantCulture),
+							UpperThreshold = (int)float.Parse(section["fThresholdMax"], CultureInfo.InvariantCulture),
+							ArmorSlotsToUnequip = section["sUnequipSlot"],
+							UnequipThreshold = (int)float.Parse(section["fThresholdUnequip"] ?? "0", CultureInfo.InvariantCulture),
+							OnlyDoctorCanReset = int.Parse(section["bOnlyDoctorCanReset"] ?? "0") == 1,
+							IsAdditive = int.Parse(section["bIsAdditive"] ?? "0") == 1,
+							HasAdditiveLimit = int.Parse(section["bHasAdditiveLimit"] ?? "0") == 1,
+							AdditiveLimit = (int)float.Parse(section["fAdditiveLimit"] ?? "0", CultureInfo.InvariantCulture)
+						};
+						sliderSet.UpdateType = sliderSet.UpdateTypeList.FirstOrDefault(it => it.Item1 == int.Parse(section["iUpdateType"]));
+						sliderSet.ApplyTo = sliderSet.ApplyToList.FirstOrDefault(it => it.Item1 == int.Parse(section["iApplyTo"] ?? "-1"));
+						sliderSet.Sex = sliderSet.SexList.FirstOrDefault(it => it.Item1 == int.Parse(section["iSex"] ?? "-1"));
+
+						settings.AddSliderSet(sliderSet);
+					}
+				}
 			}
 		}
 
